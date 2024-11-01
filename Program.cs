e49fb1fec8044a6bb4c40b8b937bdf3f55b0179f
Douglas Soares <douglassoares.cinema@gmail.com>
2024-11-01 14:32:13 -0300

    Entrega de desafio

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        static float calculaRank(int vitorias, int derrotas)
+        {
+            return vitorias - derrotas;
+        }
+
+        Console.WriteLine("Digite seu número de vitórias: ");
+        int vitorias = int.Parse(Console.ReadLine() ?? "0");
+
+        Console.Write("\n");
+
+        Console.WriteLine("Digite seu número de derrotas: ");
+        int derrotas = int.Parse(Console.ReadLine() ?? "0");
+
+        float rank = calculaRank(vitorias, derrotas);
+        string classificacao = "";
+
+        if (rank <= 10)
+        {
+            classificacao = "FERRO";
+        }
+        else if (rank <= 20)
+        {
+            classificacao = "BRONZE";
+        }
+        else if (rank <= 50)
+        {
+            classificacao = "PRATA";
+        }
+        else if (rank <= 80)
+        {
+            classificacao = "OURO";
+        }
+        else if (rank <= 90)
+        {
+            classificacao = "DIAMANTE";
+        }
+        else if (rank <= 100)
+        {
+            classificacao = "LENDÁRIO";
+        }
+        else if (rank > 100)
+        {
+            classificacao = "IMORTAL";
+        }
+
+        Console.WriteLine($@"
+        O Herói tem um saldo de {rank} e está no nível {classificacao};
+        ");
+
+    }
+}
